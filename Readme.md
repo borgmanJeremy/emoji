9f7d3da6be42877c8bafaf53f33d1977d9a91f11
Jeremy Borgman <borgman.jeremy@pm.me>
2021-12-27 14:08:42 -0600

    Working on filter icons

--- Readme.md
+++ Readme.md
@@ -2,3 +2,11 @@
 - [ ] Add category filters to bottom of widget
 - [ ] Add skin tone selector
 - [ ] Figure out how to package and reference the svg's properly
+
+## Notes
+Handy commands to filter json:
+
+Print all emoji groups:
+```
+cat index.json | jq '.[]| {group: .group}' | sort | uniq
+```
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,14 @@
 #include <QLineEdit>
 #include <QVBoxLayout>
 #include <QObject>
+#include <QPixmap>
 #include "emojiwindow.h"
+
+struct CategoryButton {
+    std::string description;
+    std::string emoji_code;
+
+};
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -16,15 +23,42 @@ int main(int argc, char *argv[]) {
     auto *v_layout = new QVBoxLayout(main_window);
     auto *line = new QLineEdit("flag", main_window);
 
+    std::vector<CategoryButton> const category_list  { {"activities", "26BD"},
+                                                 {"animals-nature", "1F63A"}};
+
+    QString icon_path = "/home/jeremy/github/emoji/assets/buttons/";
+
+    QPixmap pixmap(icon_path + "26BD.png");
+    QIcon ButtonIcon(pixmap);
+
+    std::vector <QPushButton*> button_list;
+
+    auto *buttonBox = new QWidget;
+
+    auto *button_layout = new QHBoxLayout(buttonBox);
+    for (auto const &button: category_list){
+        button_list.emplace_back(new QPushButton(QString::fromStdString(button.description)));
+    }
+
+    for (auto &button : button_list){
+        button->setIcon(pixmap);
+        button_layout->addWidget(button);
+    }
+
+
     scrollArea->setWidget(&window);
 
     v_layout->addWidget(line);
     v_layout->addWidget(scrollArea);
+    v_layout->addWidget(buttonBox);
 
     main_window->setFixedWidth(460);
     main_window->setFixedHeight(400);
 
     main_window->connect(line, &QLineEdit::textEdited,[&window, &line](){window.filter(line->text());});
+
+
+
     main_window->show();
 
     return app.exec();
